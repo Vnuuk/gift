0ff54c8d71292df08ef92730dfad09c13b2d0ccd
Anton <noginanton@gmail.com>
2015-07-21 20:19:28 +0300

    add travels from admin (fixed routes)

---
+++ NS.Elenalife.UI/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace NS.Elenalife.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public ActionResult CreateTravel()
+        {
+            return View("AddEditTravel");
+        }
+    }
+}
