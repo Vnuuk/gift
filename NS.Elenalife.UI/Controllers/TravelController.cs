60ff3228c55020a7384aa2865494a5f45ae6333d
Anton <noginanton@gmail.com>
2015-07-22 18:20:40 +0300

    prepared travel/posters page

---
+++ NS.Elenalife.UI/Controllers/TravelController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace NS.Elenalife.UI.Controllers
+{
+    public class TravelController : Controller
+    {
+        public ActionResult Posters()
+        {
+            return View("Posters");
+        }
+    }
+}
