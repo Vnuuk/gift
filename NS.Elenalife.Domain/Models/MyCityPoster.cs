667e53e3bc439a1d5e27db3755a99c5321aae006
Anton <noginanton@gmail.com>
2015-07-24 08:47:10 +0300

    added city posters; more live content.

---
+++ NS.Elenalife.Domain/Models/MyCityPoster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NS.Elenalife.Domain.Models
+{
+    public class MyCityPoster
+    {
+        public virtual Image Image { get; set; }
+        public virtual string FirstLabel { get; set; }
+        public virtual string SecondLabel { get; set; }
+        public virtual DateTime Date { get; set; }
+    }
+}
