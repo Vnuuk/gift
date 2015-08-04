1d7490f6e38e74866dd0832cd92ee086f526ced5
Anton <noginanton@gmail.com>
2015-08-04 17:48:52 +0300

    added good looking urls

--- NS.Elenalife.UI/Controllers/HomeController.cs
+++ NS.Elenalife.UI/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Web.Mvc;
-using Newtonsoft.Json.Linq;
-using NS.Elenalife.UI.Models;
+﻿using System.Web.Mvc;
 
 namespace NS.Elenalife.UI.Controllers
 {
@@ -12,28 +6,27 @@ namespace NS.Elenalife.UI.Controllers
     {
         public ActionResult Index()
         {
+            return View();   
+        }
+
+        public ActionResult Notes()
+        {
+            return View();
+        }
+
+        public ActionResult Maps()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
             return View();
         }
 
         public ActionResult Instagram()
         {
-            var result = new List<PhotoVm>();
-            using (var client = new WebClient())
-            {
-                var bytes = client.DownloadData("https://api.instagram.com/v1/users/471996769/media/recent/?access_token=35264441.8b94ff9.b401707386d646b3b0c043a4663de713");
-                var info = Encoding.ASCII.GetString(bytes);
-
-                var data = JObject.Parse(info).SelectToken("data").ToList();
-                foreach (var obj in data)
-                {
-                    var title = obj.SelectToken("caption").HasValues ? obj.SelectToken("caption").SelectToken("text").ToString() : "";
-                    var img = obj.SelectToken("images").SelectToken("standard_resolution").SelectToken("url").ToString();
-                    result.Add(new PhotoVm { Title = title, Url = img });
-                }
-
-            }
-
-            return View(result);
+            return View();
         }
     }
 }
--- NS.Elenalife.UI/Global.asax.cs
+++ NS.Elenalife.UI/Global.asax.cs
@@ -1,7 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using NS.Elenalife.Domain;
 
 namespace NS.Elenalife.UI
 {
@@ -11,11 +11,10 @@ namespace NS.Elenalife.UI
         {
             AreaRegistration.RegisterAllAreas();
 
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            Bootstrapper.Start();
         }
     }
 }
