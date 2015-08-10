880fae5b1249b14783b61a2ca7183615b27c2854
Anton <noginanton@gmail.com>
2015-08-10 13:29:42 +0300

    implemented authorization

---
+++ NS.Elenalife.UI/Controllers/AuthAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NS.Elenalife.UI.Controllers
+{
+    public class Auth : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string actionName = filterContext.ActionDescriptor.ActionName;
+            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName;
+            if(actionName.ToLower() == "login" && controllerName.ToLower() == "eladmin")
+                return;
+
+            var cookie = HttpContext.Current.Request.Cookies.Get("nselpac");
+            if (cookie == null || cookie.Value == "" || cookie.Value != "#!corspolution")
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary
+                            {
+                                {"Controller", "Eladmin"},
+                                {"Action", "Login"}
+                            });
+            }
+        }
+    }
+}
---
+++ NS.Elenalife.UI/Controllers/EladminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NS.Elenalife.UI.Controllers
+{
+    [Auth]
+    public class EladminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(string password)
+        {
+            if (password == "!#$olalekan")
+            {
+                var cookie = new HttpCookie("nselpac", "#!corspolution");
+                cookie.Expires = DateTime.Now.AddMinutes(2);
+                Response.Cookies.Add(cookie);
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Login");
+        }
+    }
+}
